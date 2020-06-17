4c0b28bf1ca844385d4cc1f66d6640fa525d134d
Shane Soh <shanesoh@gmail.com>
2020-06-17 12:27:15 +0800

    Add env var for envoy debug level

--- compose/envoy/entrypoint.sh
+++ compose/envoy/entrypoint.sh
@@ -2,4 +2,4 @@
 
 sed -e "s/\${SERVICE_NAME}/${SERVICE_NAME}/" -e "s/\${SERVICE_PORT}/${SERVICE_PORT}/" /config/envoy.yaml > /etc/envoy.yaml
 
-/usr/local/bin/envoy -c /etc/envoy.yaml
+/usr/local/bin/envoy -c /etc/envoy.yaml -l ${DEBUG_LEVEL}
